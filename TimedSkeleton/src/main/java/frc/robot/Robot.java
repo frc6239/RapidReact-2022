e12c73ccd809936cf97ec708263f32d32f223b2a
PiRadius314 <judej6706@gmail.com>
2022-02-22 20:31:32 -0500

    Configured the Falcon's integrated sensor. Tested using integrated sensor.

--- TimedSkeleton/src/main/java/frc/robot/Robot.java
+++ TimedSkeleton/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Joystick;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.*;
 
@@ -22,6 +24,10 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  final int kUnitsPerRevolution = 2048;
+  final TalonFXInvertType kInvertType = TalonFXInvertType.Clockwise;
+  final NeutralMode kBrakeDurNeutral = NeutralMode.Coast;
+  int _loops = 0;
 
   WPI_TalonFX mytalon = new WPI_TalonFX(5, "rio");
   Joystick stickLeft = new Joystick(0);
@@ -37,14 +43,53 @@ public class Robot extends TimedRobot {
     speed = 0.2;
     direction = 1.0;
     lastSpeed = 0.0;
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
+    mytalon.configAllSettings(configs);
     mytalon.configFactoryDefault();
-
     mytalon.set(ControlMode.PercentOutput, 0);
+    mytalon.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+    mytalon.setInverted(kInvertType);
+    mytalon.setNeutralMode(kBrakeDurNeutral);
 }
 
   @Override
   public void robotPeriodic() {
-  }
+    double appliedMotorOutput = mytalon.getMotorOutputPercent();
+		double selSenPos = mytalon.getSelectedSensorPosition(0); /* position units */
+		double selSenVel = mytalon.getSelectedSensorVelocity(0); /* position units per 100ms */
+
+		/* scaling depending on what user wants */
+		double pos_Rotations = (double) selSenPos / kUnitsPerRevolution;
+		double vel_RotPerSec = (double) selSenVel / kUnitsPerRevolution * 10; /* scale per100ms to perSecond */
+		double vel_RotPerMin = vel_RotPerSec * 60.0;
+
+		/*
+		 * Print to console. This is also a good oppurtunity to self-test/plot in Tuner
+		 * to see how the values match.
+		 * 
+		 * Note these prints can cause "Loop time of 0.02s overrun" errors in the console.
+		 * This is because prints are slow.
+		 */
+		if ((++_loops >= 10) && (stopped == false)) {
+			_loops = 0;
+			System.out.printf("Motor-out: %.2f | ", appliedMotorOutput);
+			System.out.printf("Pos-units: %.2f | ", selSenPos);
+			System.out.printf("Vel-unitsPer100ms: %.2f | ", selSenVel);
+			System.out.printf("Pos-Rotations:%.3f | ", pos_Rotations);
+			System.out.printf("Vel-RPS:%.1f | ", vel_RotPerSec);
+			System.out.printf("Vel-RPM:%.1f | ", vel_RotPerMin);
+			System.out.println();
+		}
+
+		/* set position to zero on button 1 */
+		if (stickLeft.getRawButtonPressed(6)) {
+			mytalon.setSelectedSensorPosition(0);
+      System.out.println();
+      System.out.println();
+      System.out.println();
+		}
+	}
 
   @Override
   public void autonomousInit() {
