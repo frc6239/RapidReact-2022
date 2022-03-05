4539715359235261f09537fa0a55b894f65122e4
PiRadius314 <judej6706@gmail.com>
2022-03-05 13:17:04 -0500

    Removed the resetSensor method. Added a variable to track when we start lowering. Simplified the isIntakeLowered method. Cleaned up some variables.

--- CompBot2022/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ CompBot2022/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -39,7 +39,6 @@ public class AutonomousCommand extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        m_intakeSystem.resetSensor();
         intakeLowered = false;
     }
 
